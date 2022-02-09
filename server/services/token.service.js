8b67e4719dd3b6c76d3fca2d2138d996550865fc
Denis Bugreev <den9.rcq@gmail.com>
2022-02-09 18:06:27 +0300

    auth, reg &  refresh

---
+++ server/services/token.service.js
@@ -0,0 +1,47 @@
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const Token = require('../models/Token')
+
+class TokenService {
+    generate(payload) {
+        const accessToken = jwt.sign(payload, config.get('accessSecret'), {
+            expiresIn: '1h'
+        })
+
+        const refreshToken = jwt.sign(payload, config.get('refreshSecret'))
+
+        return {
+            accessToken,
+            refreshToken,
+            expiresIn: 3600
+        }
+    }
+
+    async save(userId, refreshToken) {
+        const data = await Token.findOne({ user: userId })
+        if (data) {
+            data.refreshToken = refreshToken
+            return data.save()
+        }
+
+        return await Token.create({ user: userId, refreshToken })
+    }
+
+    validateRefresh(refreshToken) {
+        try {
+            return jwt.verify(refreshToken, config.get('refreshSecret'))
+        } catch (e) {
+            return null
+        }
+    }
+
+    async findToken(refreshToken) {
+        try {
+            return await Token.findOne({ refreshToken })
+        } catch (e) {
+            return null
+        }
+    }
+}
+
+module.exports = new TokenService()
