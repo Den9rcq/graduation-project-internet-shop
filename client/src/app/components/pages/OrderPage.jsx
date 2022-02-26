1877e071188ae080a8adc1edd9da8864dafad5d6
Denis Bugreev <den9.rcq@gmail.com>
2022-02-26 15:11:31 +0300

    css link

--- client/src/app/components/pages/OrderPage.jsx
+++ client/src/app/components/pages/OrderPage.jsx
@@ -6,8 +6,10 @@ const OrderPage = () => {
         <div className="flex-center mt-7">
             <h4>Это не настоящая страница оплаты.</h4>
             <h5>Сайт создан для демонстрации кода</h5>
-            <a className="btn red mt-2 mb-2" href="https://github.com/Den9rcq/graduation-project-internet-shop">ссылка проекта на GitHub</a>
-            <Link to='/'>вернуться на главную</Link>
+            <a
+                className="btn red mt-2 mb-2"
+                href="https://github.com/Den9rcq/graduation-project-internet-shop">ссылка проекта на GitHub</a>
+            <Link to='/'> <span className="btn blue">вернуться на главную </span></Link>
         </div>
     );
 };
